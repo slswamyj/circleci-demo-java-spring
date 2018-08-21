72aadc5e2b305de2761da64ab82ec626d44a4586
slswamyj <slswamy.j2014@gmail.com>
2018-08-21 22:37:25 +0530

    making silly mistake

--- src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
+++ src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJavaSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoJavaSpringApplication.class, args);
+		SpringApplication.run(DemoJavaSpringApplication.class, args1);
 	}
 }
